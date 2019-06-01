57dd6bdd8855e197e66a6cc125896fcd40953e7b
yuanxs <yuanxs@95szzl.cn>
2019-06-01 20:24:31 +0800

    0.0.3

--- global/Global.go
+++ global/Global.go
@@ -8,7 +8,7 @@ import (
 const (
 	//PreVersion = "0.0.2 Build20190531"
 	//TestVersion = "0.0.0 Build20190101"
-	Version = "0.0.0 Build20190101"
+	Version = "0.0.3 Build20190601"
 )
 
 var Ctx context.Context
